fee731d5427cb9e76e552b170933e849a49024fc
Sergio Labora <sergio.labora@gmail.com>
2021-05-17 15:01:55 +0200

    added to_string() method to block statement

--- syntax/block.py
+++ syntax/block.py
@@ -9,3 +9,13 @@ class Block(Statement):
     
     def add_statement(self, stt):
         self.statements.append(stt)
+    
+    def to_string(self):
+        result = '{\n'
+
+        for stt in self.statements:
+            result += '\t' * stt.indent + f'{stt.to_string()};\n'
+
+        result += '}\n'
+
+        return result
